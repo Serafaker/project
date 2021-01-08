aaf7cea08d8efb53a59b30e8c479fa43fe97a8db
SergioK <serafaker@gmail.com>
2021-01-08 18:32:38 +0300

    second

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-"use strict"
+"use strict";
 
 //const result = confirm ("are you here?");
 
 //console.log(result);
-const answer = +prompt("есть 18?", "");
+const answer = +prompt("есть 18?", "18");
 console.log(typeof(answer));
