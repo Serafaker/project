873ce38862de8014492909c5cdcac77d49d132c1
SergioK <serafaker@gmail.com>
2021-01-11 00:35:14 +0300

    практика 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-"use strict";
 
-const numberoffilms = +prompt("Сколько фильмов ты смотрел?", "");
+
+let numberoffilms = prompt("Сколько фильмов ты смотрел?", "");
 
 const personallist = {
     count: numberoffilms,
@@ -9,13 +9,28 @@ const personallist = {
     genres: [],
     privat: false
 };
+if (numberoffilms <=10){
+    console.log("Смотри больше!");
+    } else if (numberoffilms <=30){
+        console.log("Норм!");
+    } else if (numberoffilms >30){
+        console.log("100% смотрел чел многоножку!");
+    }   else if (numberoffilms !==1){
+        console.log("и правильно порно это не кино");
+    
+}
 
-const a = prompt("какой фильм ты смотрел?");
-const b = +prompt("на сколько баллов фильм?");
-const c = prompt("какой фильм ты смотрел?");
-const d = +prompt("на сколько баллов фильм?");
 
-personallist.movies[a] = b;
-personallist.movies[c] = d;
+for (i=0; i<2; i++) {
+    let a = prompt("какой фильм ты смотрел?"),
+        b = prompt("На сколько  баллов фильм?");
+
+        if (a !=null && b !=null && a.length<50 && b.length<50 && a !="" && b !=""){
+        personallist.movies[a] = b;
+        } else {
+        console.log("Хуеча!! давай по новой!");
+        i--;
+        }
+}
 
 console.log(personallist);
